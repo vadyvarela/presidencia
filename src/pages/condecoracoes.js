34dbd2d2b18f7a40732e67ad02efa17031b5bf99
Ebrailson Gabriel <ebrailsongil@hotmail.com>
2021-11-03 09:46:29 -0100

    add photos

--- src/pages/condecoracoes.js
+++ src/pages/condecoracoes.js
@@ -6,6 +6,7 @@ import Header from '../components/Main/Header'
 import Footer from '../components/Main/Footer'
 import { Fancybox } from "@fancyapps/ui"
 import "@fancyapps/ui/dist/fancybox.css";
+
 const myLoader = ({ src, width, quality }) => {
   return `${src}?w=${width}&q=${quality || 25}`
 }
@@ -29,12 +30,27 @@ var gallery = [
   },
 ];
 
+// function openImage() {
+//   Fancybox.show(gallery, {
+//     // Your options go here
+//   });
+// }
+var list1mandato = [];
+for (var i = 1; i <= 215; i++) {
+  list1mandato.push({
+    src: "./img/condecoracao/1mandato/con(" + i + ").jpg",
+    // thumb: "./img/africa01" + i + ".jpg",
+    caption: "",
+  });
+}
+console.log(list1mandato)
+
+
 function openImage() {
-  Fancybox.show(gallery, {
+  Fancybox.show(list1mandato, {
     // Your options go here
   });
 }
-
 /*Fancybox.bind("[data-fancybox]", {
   // Your options go here
 });*/
@@ -44,44 +60,44 @@ export default function Condecoracoes() {
     <Layout title="Condecorações" description={`Condecorações`}>
       <section className="h-screen w-screen max-w-screen-lg xl:max-w-screen-xl container mx-auto px-4" >
         <Header />
-        
+
         <div className="mt-4 ">
           <h2 className="mt-8 text-3xl md:text-4xl font-semibold text-textcolor leading-tight"> Condecorações </h2>
 
           <div className="mb-32 mt-5">
             <div className="shadowpr mb-8 ">
               <Image
-              alt="Condecorações"
-              src={condecoracoes}
-              objectFit="cover"
-              priority={true}
-              loader={myLoader}
-              placeholder='blur'
-              blurDataURL={condecoracoes}
+                alt="Condecorações"
+                src={condecoracoes}
+                objectFit="cover"
+                priority={true}
+                loader={myLoader}
+                placeholder='blur'
+                blurDataURL={condecoracoes}
               />
             </div>
 
             <p className="mr-6 text-base text-textcolor leading-relaxed">
-            Jorge Carlos Fonseca definiu o Dia Nacional da Cultura e das Comunidades – 18 de Outubro - como efeméride especial 
-            para distinguir, em vida ou a título póstumo, cidadãos cabo-verdianos no exterior, que se notabilizem 
-            por méritos pessoais, por feitos cívicos ou militares ou por serviços prestados ao País e nas comunidades onde 
-            estão inseridos. <br /><br />
+              Jorge Carlos Fonseca definiu o Dia Nacional da Cultura e das Comunidades – 18 de Outubro - como efeméride especial
+              para distinguir, em vida ou a título póstumo, cidadãos cabo-verdianos no exterior, que se notabilizem
+              por méritos pessoais, por feitos cívicos ou militares ou por serviços prestados ao País e nas comunidades onde
+              estão inseridos. <br /><br />
 
-            No uso da competência que lhe é conferida por lei, e em reconhecimento do contributo inestimável e do esforço meritório 
-            de alguns cidadãos cabo-verdianos das comunidades emigradas, bem assim nacionais dos países de acolhimento que se têm 
-            destacado pela atenção dada às nossas comunidades emigradas, facilitando a sua integração e reforçando os laços de 
-            cooperação entre os Cabo Verde e esses países, Jorge Carlos Fonseca atribuiu medalhas a cidadãos cabo-verdianos e 
-            descendentes, a estrangeiros e a instituições e organizações em África, na Europa e nos Estados Unidos, informação 
-            disponível nos Decretos-presidenciais publicados entre os anos 2013 e 2020. <br /><br />
+              No uso da competência que lhe é conferida por lei, e em reconhecimento do contributo inestimável e do esforço meritório
+              de alguns cidadãos cabo-verdianos das comunidades emigradas, bem assim nacionais dos países de acolhimento que se têm
+              destacado pela atenção dada às nossas comunidades emigradas, facilitando a sua integração e reforçando os laços de
+              cooperação entre os Cabo Verde e esses países, Jorge Carlos Fonseca atribuiu medalhas a cidadãos cabo-verdianos e
+              descendentes, a estrangeiros e a instituições e organizações em África, na Europa e nos Estados Unidos, informação
+              disponível nos Decretos-presidenciais publicados entre os anos 2013 e 2020. <br /><br />
 
-            A 18 de Outubro de 2015, o ano do 40º aniversário da Independência Nacional, foram condecorados 40 cidadãos que, pela 
-            exemplaridade assim publicamente demonstrada, constituem-se em factor motivador e criador de estímulos numa sociedade 
-            que defende os valores da civilização. <br /><br />
+              A 18 de Outubro de 2015, o ano do 40º aniversário da Independência Nacional, foram condecorados 40 cidadãos que, pela
+              exemplaridade assim publicamente demonstrada, constituem-se em factor motivador e criador de estímulos numa sociedade
+              que defende os valores da civilização. <br /><br />
 
-            As medalhas foram entregues pelo próprio Presidente da República nas visitas que fez às comunidade cabo-verdianas, 
-            normalmente em eventos solenes com enorme significado para os distinguidos e para os participantes. 
-            <br /><br /> É imperativo para Jorge Carlos Fonseca que, sendo possível, as cerimónias de condecoração de personalidades das 
-            comunidades cabo-verdianas sejam realizadas nos países onde elas residem. <br /><br />
+              As medalhas foram entregues pelo próprio Presidente da República nas visitas que fez às comunidade cabo-verdianas,
+              normalmente em eventos solenes com enorme significado para os distinguidos e para os participantes.
+              <br /><br /> É imperativo para Jorge Carlos Fonseca que, sendo possível, as cerimónias de condecoração de personalidades das
+              comunidades cabo-verdianas sejam realizadas nos países onde elas residem. <br /><br />
 
             </p>
 
@@ -92,13 +108,13 @@ export default function Condecoracoes() {
             </div>
           </div>
 
-         
+
         </div>
-        <Footer 
+        <Footer
           title1={<a className="text-base text-textcolor opacity-50 font-bold">Introdução </a>} link1="/introducao"
           title2={<a className="text-base text-textcolor font-bold"> Condecorações </a>} link2="/condecoracoes"
           title3={<a className="font-bold text-base text-textcolor opacity-50"> Citações </a>} link3="/citacoes"
-          />
+        />
       </section>
     </Layout>
   )
