220fae2d2c0891a0f596d4d5a570ba234276607f
Ebrailson Gabriel <ebrailsongil@hotmail.com>
2021-11-03 23:03:14 -0100

    add photos to galery section

--- src/pages/galeria.js
+++ src/pages/galeria.js
@@ -6,70 +6,63 @@ const myLoader = ({ src, width, quality }) => {
   return `${src}?w=${width}&q=${quality || 25}`
 }
 
-var gallery = [];
-for (var i = 1; i <= 9; i++) {
-  gallery.push({
-    src: "./img/europa/" + i + ".jpg",
+var galleryPortugal = [];
+for (var i = 1; i <= 314; i++) {
+  galleryPortugal.push({
+    src: "./img/galeria/portugal/portugal(" + i + ").jpg",
     // thumb: "./img/africa01" + i + ".jpg",
-    caption: "",
+    caption: "Portugal",
   });
 }
 
 
-var galleryeua = [];
-for (var i = 1; i <= 9; i++) {
-  galleryeua.push({
-    src: "./img/eua/" + i + ".jpg",
+var galleryEua = [];
+for (var i = 1; i <= 346; i++) {
+  galleryEua.push({
+    src: "./img/galeria/eua/eua(" + i + ").jpg",
     // thumb: "./img/africa01" + i + ".jpg",
-    caption: "",
+    caption: "Estados Unidos",
   });
 }
 
 var gallerybrazil = [];
-for (var i = 1; i <= 9; i++) {
+for (var i = 1; i <= 59; i++) {
   gallerybrazil.push({
-    src: "./img/brazil/" + i + ".jpg",
+    src: "./img/galeria/brazil/brazil(" + i + ").jpg",
     // thumb: "./img/africa01" + i + ".jpg",
-    caption: "",
+    caption: "Brazil",
   });
 }
 
-var galleryafrica = [];
-for (var i = 1; i <= 9; i++) {
-  galleryafrica.push({
-    src: "./img/africa01/" + i + ".jpg",
+var galleryAngola = [];
+for (var i = 3; i <= 31; i++) {
+  galleryAngola.push({
+    src: "./img/galeria/angola/angola(" + i + ").jpg",
     // thumb: "./img/africa01" + i + ".jpg",
-    caption: "",
+    caption: "Angola",
   });
 }
 
-var galleryafrica2 = [];
-for (var i = 1; i <= 9; i++) {
-  galleryafrica2.push({
-    src: "./img/africa02/" + i + ".jpg",
+var gallerySaoTome = [];
+for (var i = 1; i <= 58; i++) {
+  gallerySaoTome.push({
+    src: "./img/galeria/saotome/saotome(" + i + ").jpg",
     // thumb: "./img/africa01" + i + ".jpg",
-    caption: "",
+    caption: "São Tomé",
   });
 }
 
-var galleryafrica3 = [];
-for (var i = 1; i <= 9; i++) {
-  galleryafrica3.push({
-    src: "./img/africa03/" + i + ".jpg",
+var gallerySenegal = [];
+for (var i = 1; i <= 50; i++) {
+  gallerySenegal.push({
+    src: "./img/galeria/senegal/senegal(" + i + ").jpg",
     // thumb: "./img/africa01" + i + ".jpg",
-    caption: "",
+    caption: "Senegal",
   });
 }
 
-
-function openImage02() {
-  Fancybox.show(gallery, {
-    // Your options go here
-  });
-}
-
-function openImageeua() {
-  Fancybox.show(galleryeua, {
+function openImagePortugal() {
+  Fancybox.show(galleryPortugal, {
     // Your options go here
   });
 }
@@ -79,19 +72,23 @@ function openImageBrazil() {
     // Your options go here
   });
 }
-
-function openImageAfrica1() {
-  Fancybox.show(galleryafrica, {
+function galleryeua() {
+  Fancybox.show(galleryEua, {
     // Your options go here
   });
 }
-function openImageAfrica2() {
-  Fancybox.show(galleryafrica2, {
+function openImageAngola() {
+  Fancybox.show(galleryAngola, {
     // Your options go here
   });
 }
-function openImageAfrica3() {
-  Fancybox.show(galleryafrica3, {
+function openImageSaoTome() {
+  Fancybox.show(gallerySaoTome, {
+    // Your options go here
+  });
+}
+function openImageSenegal() {
+  Fancybox.show(gallerySenegal, {
     // Your options go here
   });
 }
@@ -106,34 +103,34 @@ export default function Galeria() {
           <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 1920 1080" preserveAspectRatio="slice" >
 
             <image className="w-full flex justify-center items-center bg-contain" xlinkHref="./img/gallerys.png" />
+            <g className="hover_group" opacity="0">
+              <a className="cursor-pointer" onClick={openImageBrazil} >
+                <circle cx="690" cy="706" r="25" fill="#fff0" width="264.6" height="387.8"></circle>
+              </a>
+            </g>
 
             <g className="hover_group" opacity="0">
-              <a onClick={openImageeua} >
+              <a className="cursor-pointer" onClick={galleryeua} >
                 <circle cx="613" cy="552" r="25" fill="#fff0" width="264.6" height="387.8"></circle>
               </a>
             </g>
             <g className="hover_group" opacity="0">
-              <a onClick={openImageBrazil} >
-                <circle cx="690" cy="706" r="25" fill="#fff0" width="264.6" height="387.8"></circle>
-              </a>
-            </g>
-            <g className="hover_group" opacity="0">
-              <a onClick={openImage02} >
+              <a className="cursor-pointer" onClick={openImagePortugal} >
                 <circle cx="896" cy="514" r="25" fill="#fff0" width="264.6" height="387.8"></circle>
               </a>
             </g>
             <g className="hover_group" opacity="0">
-              <a onClick={openImageAfrica3} >
+              <a className="cursor-pointer" onClick={openImageSaoTome} >
                 <circle cx="860" cy="650" r="25" fill="#fff0" width="264.6" height="387.8"></circle>
               </a>
             </g>
             <g className="hover_group" opacity="0">
-              <a onClick={openImageAfrica2}  >
+              <a className="cursor-pointer" onClick={openImageSenegal}  >
                 <circle cx="965" cy="710" r="25" fill="#fff0" width="264.6" height="387.8"></circle>
               </a>
             </g>
             <g className="hover_group" opacity="0">
-              <a onClick={openImageAfrica1} >
+              <a className="cursor-pointer" onClick={openImageAngola} >
                 <circle cx="1055" cy="842" r="25" fill="#fff0" width="264.6" height="387.8"></circle>
               </a>
             </g>
